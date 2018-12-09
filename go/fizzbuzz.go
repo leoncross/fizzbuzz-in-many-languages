fcfe7a9b5ff92c80f9257e8d33ab948a855911d6
leoncross <leon@lscross.co.uk>
2018-12-09 12:15:37 +0000

    refactored; moved fizzbuzz higher in if statement

--- go/fizzbuzz.go
+++ go/fizzbuzz.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func fizzbuzz(n int) string {
-	if n == 3 {
-		return "Fizz"
+	if n == 15 {
+		return "Fizzbuzz"
 	} else if n == 5 {
 		return "Buzz"
-	} else if n == 15 {
-		return "Fizzbuzz"
+	} else if n == 3 {
+		return "Fizz"
 	} else {
 		return fmt.Sprintf("%d", n)
 	}
